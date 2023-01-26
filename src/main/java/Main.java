066011074d68452224902b126e42daedac8d46c7
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-26 13:04:30 +0100

    pierwszy commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +11,50 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+        List<Integer> numbers = new ArrayList<>();
+        boolean condition = true;
+        int a;
+        while (condition) {
+            a = scanner.nextInt();
+            if (a > 0) {
+                numbers.add(a);
+            } else {
+                condition = false;
+            }
+        }
+        showReverseList(numbers);
+        showSum(numbers);
+        showMinNumber(numbers);
+        showMax(numbers);
+    }
+
+    private void showMax(List<Integer> numbers) {
+        Collections.sort(numbers);
+        System.out.println("Największa liczba w liście to " + numbers.get(numbers.size() - 1));
+    }
+
+    private void showMinNumber(List<Integer> numbers) {
+        Collections.sort(numbers);
+        System.out.println("Najmniejsza liczba w liście to " + numbers.get(0));
+    }
+
+    private void showSum(List<Integer> numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            System.out.print(numbers.get(i) + " + ");
+        }
+        System.out.print(numbers.get(numbers.size() - 1) + " = " + sum);
+        System.out.println();
+    }
+
+    private void showReverseList(List<Integer> numbers) {
+        for (int i = numbers.size() - 1; i > 0; i--) {
+            System.out.print(numbers.get(i) + ", ");
+        }
+        System.out.print(numbers.get(0));
+        System.out.println();
     }
 }
